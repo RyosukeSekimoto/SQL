71926148be358a2d97403615fc1d350e0ec2be47
ryosuke sekimoto <shojiniyan@gmail.com>
2017-11-22 18:24:08 +0900

    Q5まで作成

---
+++ Q2.sql
@@ -0,0 +1,6 @@
+CREATE TABLE item (
+    item_id int PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    item_name varchar(256) NOT NULL,
+    item_price int NOT NULL,
+    category_id int
+);
---
+++ Q3.sql
@@ -0,0 +1,5 @@
+INSERT INTO item_category VALUES (1, '家具');
+INSERT INTO item_category VALUES (2, '食品');
+INSERT INTO item_category VALUES (1, '本');
+
+
