1991d70167258b92f0e9a86e3eda0da993c8d2c6
ryosuke sekimoto <shojiniyan@gmail.com>
2017-11-26 15:04:24 +0900

    Q9まで作成

---
+++ Q9.sql
@@ -0,0 +1,7 @@
+SELECT
+    item_category.category_name, SUM(item.item_price) AS total_price
+FROM
+    item INNER JOIN item_category
+    ON item_category.category_id = item.category_id
+GROUP BY item.category_id
+ORDER BY total_price DESC;
